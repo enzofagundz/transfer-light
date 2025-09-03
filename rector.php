dc3b5f89cfa7eacd513da132acb5c31ea1ee5c9b
Enzo Fagundes <enzo.fagundes014@gmail.com>
2025-09-03 08:50:44 -0300

    build: Adds a Rector for refactoring automation

---
+++ rector.php
@@ -0,0 +1,13 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+
+return RectorConfig::configure()
+    ->withRules([
+        TypedPropertyFromStrictConstructorRector::class
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true
+    );
