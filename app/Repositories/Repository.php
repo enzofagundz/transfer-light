2950222e056c1698a2ff5966178a5724188e203a
Enzo Fagundes <enzo.fagundes014@gmail.com>
2025-09-03 17:40:24 -0300

    feat: Adds base structure for repositories

---
+++ app/Repositories/Repository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Interfaces\BaseRepository;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class Repository implements BaseRepository
+{
+    protected Model $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function find($id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        return $this->model->find($id)->update($data);
+    }
+
+    public function delete($id)
+    {
+        return $this->model->find($id)->delete();
+    }
+}
