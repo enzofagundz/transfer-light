dd4c0725da7609abeb3973d79756af9d525aa698
Enzo Fagundes <enzo.fagundes014@gmail.com>
2025-09-03 14:01:33 -0300

    feat: Adds Type and CPF_CNPJ fields to UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserType;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -29,6 +30,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'type' => $this->faker->randomElement([UserType::Common, UserType::Merchant]),
+            'cpf_cnpj' => $this->faker->unique()->numerify('###.###.###-##'),
         ];
     }
 
