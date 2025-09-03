8981f5573b064a9890be3b723fd4524198440903
Enzo Fagundes <enzo.fagundes014@gmail.com>
2025-09-03 13:51:26 -0300

    feat: Adds paths for Rector verification

--- rector.php
+++ rector.php
@@ -4,6 +4,12 @@ use Rector\Config\RectorConfig;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 
 return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/database',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ])
     ->withRules([
         TypedPropertyFromStrictConstructorRector::class,
     ])
