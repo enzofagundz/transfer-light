6d8d2c6cc23d7c8eced46b7a94d1954c6edde59c
Enzo Fagundes <enzo.fagundes014@gmail.com>
2025-09-04 15:43:07 -0300

    feat: Add AuthorizeService

--- app/Providers/SystemServiceProvider.php
+++ app/Providers/SystemServiceProvider.php
@@ -9,7 +9,13 @@ class SystemServiceProvider extends ServiceProvider
     /**
      * Register services.
      */
-    public function register(): void {}
+    public function register(): void
+    {
+        $this->app->bind(
+            \App\Services\Interfaces\AuthorizeServiceInterface::class,
+            \App\Services\AuthorizeService::class
+        );
+    }
 
     /**
      * Bootstrap services.
