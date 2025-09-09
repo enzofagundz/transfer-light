368034a25cc36d400da580fede5876484ff2def2
Enzo Fagundes <enzo.fagundes014@gmail.com>
2025-09-09 15:59:02 -0300

    feat:m Add component to capture notifications

---
+++ app/Livewire/Notifications/Bell.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Livewire\Notifications;
+
+use App\Services\Interfaces\NotificationServiceInterface;
+use Illuminate\View\View;
+use Livewire\Component;
+
+class Bell extends Component
+{
+    public $unreadNotificationsCount = 0;
+
+    public function mount(NotificationServiceInterface $notificationService): void
+    {
+        $this->refreshNotifications($notificationService);
+    }
+
+    public function refreshNotifications(NotificationServiceInterface $notificationService): void
+    {
+        $this->unreadNotificationsCount = $notificationService->getUnreadCount();
+    }
+
+    public function getNotificationsProperty(NotificationServiceInterface $notificationService)
+    {
+        return $notificationService->getUnreadNotifications();
+    }
+
+    public function markAllAsRead(NotificationServiceInterface $notificationService): void
+    {
+        $notificationService->markAllAsRead();
+        $this->refreshNotifications($notificationService);
+    }
+
+    public function render(): View
+    {
+        return view('livewire.notifications.bell');
+    }
+}
